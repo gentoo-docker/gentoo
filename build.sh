dcb1d49eac74c6658d4c391b17f0e232deb8787b
Axel Etcheverry <euskadi31@gmail.com>
2015-01-28 11:18:32 +0100

    fix(build): portage func

--- build.sh
+++ build.sh
@@ -10,44 +10,22 @@ busybox wget "${dist}/${stage3}" -qO- \
 
 env-update
 
-source "${PORTAGE_BIN_PATH:-/usr/lib/portage/bin}/isolated-functions.sh"
-
-ebegin "Config timezone"
 echo 'UTC' > /etc/timezone
-eend $?
 
-ebegin "Copy make.conf"
 cp /_files/etc/portage/make.conf /etc/portage/make.conf
-eend $?
 
-ebegin "Copy cpu.conf"
 cp /_files/etc/portage/cpu.conf /etc/portage/cpu.conf
-eend $?
 
-ebegin "Remove doc"
 rm -rf /usr/share/doc
-eend $?
 
-ebegin "Remove info"
 rm -rf /usr/share/info
-eend $?
 
-ebegin "Remove man"
 rm -rf /usr/share/man
-eend $?
 
-ebegin "Remove gtk-doc"
 rm -rf /usr/share/gtk-doc
-eend $?
 
-ebegin "Remove _files"
 rm -rf /_files/
-eend $?
 
-ebegin "Remove Dockerfile"
 rm /Dockerfile
-eend $?
 
-ebegin "Remove build.sh"
 rm /build.sh
-eend $?
